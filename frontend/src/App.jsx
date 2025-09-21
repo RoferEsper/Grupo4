55133ab0558f6651d81aedbb241dea1068b4c988
Laura43773 <lauramargaritacopa@gmail.com>
2025-09-20 21:50:10 -0300

    Frontend, realizados por Belen Zamorano y Tamara Leguizamon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { 
+  HOME, CURSOSPAGES, CURSOSPAGE_CREAR, CURSOSPAGE_EDITAR, CURSOSPAGES_VER,
+  ESTUDIANTESPAGES, ESTUDIANTESPAGE_VER, ESTUDIANTESPAGE_CREAR, ESTUDIANTESPAGE_EDITAR,
+  INSCRIPCIONESPAGE 
+} from './Routers/router';
 
+// Home
+import Home from './Pages/Home';
+
+// Cursos
+import CursosPages from './Pages/CursosPages';
+import CursosPages_crear from './Pages/CursosPages_crear';
+import CursosPages_editar from './Pages/CursosPages_editar';
+import CursosPages_ver from './Pages/CursosPages_ver';
+
+// Estudiantes
+import EstudiantesPages from './Pages/EstudiantesPages';
+import EstudiantesPages_Crear from './Pages/EstudiantesPages_Crear';
+import EstudiantesPages_Editar from './Pages/EstudiantesPages_Editar';
+import EstudiantesPages_Ver from './Pages/EstudiantesPages_Ver';
+
+// Inscripciones
+import InscripcionesPages_crear from './Pages/InscripcionesPages_crear';
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        {/* Home */}
+        <Route path={HOME} element={<Home />} />
 
-export default App
+        {/* Cursos */}
+        <Route path={CURSOSPAGES} element={<CursosPages />} />
+        <Route path={CURSOSPAGE_CREAR} element={<CursosPages_crear />} />
+        <Route path={`${CURSOSPAGE_EDITAR}/:id`} element={<CursosPages_editar />} />
+        <Route path={CURSOSPAGES_VER} element={<CursosPages_ver />} />       
+      
+        
+            {/* Lista todos */}
+        <Route path={`${CURSOSPAGES_VER}/:id`} element={<CursosPages_ver />} />  {/* Detalle */}
+
+        {/* Estudiantes */}
+        <Route path={ESTUDIANTESPAGES} element={<EstudiantesPages />} />
+        <Route path={ESTUDIANTESPAGE_CREAR} element={<EstudiantesPages_Crear />} />
+        <Route path={`${ESTUDIANTESPAGE_EDITAR}/:id`} element={<EstudiantesPages_Editar />} />
+        <Route path={ESTUDIANTESPAGE_VER} element={<EstudiantesPages_Ver />} />           {/* Lista todos */}
+        <Route path={`${ESTUDIANTESPAGE_VER}/:id`} element={<EstudiantesPages_Ver />} />  {/* Detalle */}
+
+        {/* Inscripciones */}
+        <Route path={`${INSCRIPCIONESPAGE}/:id`} element={<InscripcionesPages_crear />} />
+        <Route path={INSCRIPCIONESPAGE} element={<InscripcionesPages_crear />} /> 
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
--- frontend/src/Routers/router.js
+++ frontend/src/Routers/router.js
@@ -1,1 +1,26 @@
-// aqui va el ruteo
+
+
+   // CURSOS 
+export const HOME = '/'
+
+
+export const CURSOSPAGES = '/cursos'
+export const CURSOSPAGE_CREAR = '/crearcursos'
+export const CURSOSPAGE_EDITAR ='/editarcursos'
+export const CURSOSPAGES_VER = "/vercursos"
+
+//ESTUDIANTES 
+
+export const ESTUDIANTESPAGES ='/estudiantes'
+export const ESTUDIANTESPAGE_VER='/verestudiantes'
+export const ESTUDIANTESPAGE_CREAR= '/crearestudiante'
+export const ESTUDIANTESPAGE_EDITAR='/editarestudiante'
+
+
+
+
+// INSCRIOCIONES
+
+export const INSCRIPCIONESPAGE='/inscripcion'
+
+
